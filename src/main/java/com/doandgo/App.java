f1fb9fdc51e1db51944516af38ad475020f049c7
flodro34 <f.derosso@gmail.com>
2023-11-24 15:07:35 +0100

    commit Live 7

--- src/main/java/com/doandgo/App.java
+++ src/main/java/com/doandgo/App.java
@@ -1,20 +1,48 @@
 package com.doandgo;
 
 import com.doandgo.pojo.Energie;
+import com.doandgo.pojo.Garage;
 import com.doandgo.pojo.Voiture;
 import com.doandgo.pojo.VoitureElectrique;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App 
 {
     public static void main( String[] args )
     {
         Voiture maVoiture = new Voiture();
-        System.out.println( maVoiture );
-        maVoiture.setEnergie(Energie.GAZ);
-        System.out.println(maVoiture);
+        VoitureElectrique maVoitureElectrique = new VoitureElectrique();
 
-        VoitureElectrique mavoitureEectrique = new VoitureElectrique();
-        System.out.println(mavoitureEectrique);
+        List maListe = new ArrayList();
+        maListe.add(maVoitureElectrique);
+        maListe.add(maVoiture);
+
+        Garage garage = new Garage((maListe));
+
+        //Je veux récupérer toutes les voitures electriques de mon garage
+        List<Voiture> garages = garage.getLesVoitures();
+
+        //Programmation impérative
+        // à éviter car c'est bloquant
+//        for (Voiture voiture : garages){
+//            if(voiture.getEnergie().equals(Energie.ELECTRIQUE)){
+//                System.out.println(voiture);
+//            }
+//        }
+
+        //Programmation réactive
+//        System.out.println(garages
+//                .stream()
+//                .anyMatch(voiture -> voiture.isState())
+//                );
+
+            System.out.println(garages
+                    .stream()
+                    .filter(voiture -> voiture.isState())
+                    .collect(Collectors.toList()));
 
     }
 }
